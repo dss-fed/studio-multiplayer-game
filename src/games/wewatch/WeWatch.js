181c0d4593661cb6357dad66a92e9baef91ff48a
Joe Tessler <jtessler@users.noreply.github.com>
2020-04-06 18:15:19 -0400

    Add dummy YouTube video embed
    TODO: Fix height styling

--- src/games/wewatch/WeWatch.js
+++ src/games/wewatch/WeWatch.js
@@ -1,8 +1,22 @@
 import GameComponent from "../../GameComponent.js";
 import React from "react";
+import "./wewatch.css";
 
 export default class WeWatch extends GameComponent {
   render() {
-    return "Hello, World!";
+    return (
+      <div className="container">
+        <div className="box">
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/fH3X2U9t2P0?controls=0&autoplay=1"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          />
+        </div>
+      </div>
+    );
   }
 }
