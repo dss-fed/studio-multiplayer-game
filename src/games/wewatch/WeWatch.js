a17b153f9f19c4c7a22808c5a03b8c2e21b4be6e
Joe Tessler <jtessler@users.noreply.github.com>
2020-04-06 17:53:27 -0400

    Add new WeWatch component

---
+++ src/games/wewatch/WeWatch.js
@@ -0,0 +1,8 @@
+import GameComponent from "../../GameComponent.js";
+import React from "react";
+
+export default class WeWatch extends GameComponent {
+  render() {
+    return "Hello, World!";
+  }
+}
